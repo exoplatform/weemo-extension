39e4c40d20235fbdd4d4bd4ee215f829afdc8562
haquangtan <tanhq@exoplatform.com>
2014-01-19 02:00:56 +0700

    Implement user selector for video calls admin portlet

--- weemo-extension-webapp/src/main/webapp/js/Utils.js
+++ weemo-extension-webapp/src/main/webapp/js/Utils.js
@@ -86,8 +86,95 @@ function Utils() {
 	$(modalFooter).append(closeButton);
 	$(modalFooter).append(addButton);*/
 
+	
+        var listUsers = $("#UIListUsers");
+        var tbody = $('<tbody/>');
+        $(listUsers).append(tbody);
+
+        $.each(data, function (index, value) {
+	  var obj = jQuery.parseJSON(value);
+          var userName = obj.userName;
+          var firstName = obj.firstName;
+          var lastName = obj.lastName;
+          var email = obj.email;
+          var tr = $('<tr/>');
+          $(tbody).append(tr);
+	  var td = $('<td/>', {
+	    'class':'center'
+	  });
+	  $(tr).append(td);
+	  var span = $('<span/>', {
+	    'class':'uiCheckbox'
+	  });
+	  $(td).append(span);
+	  var input = $('<input/>', {
+	    'class':'checkbox',
+	    'type':'checkbox',
+            'name':userName,
+            'id':userName
+	  });
+	  $(span).append(input);
+          
+ 	  var td2 = $('<td/>');
+	  var span2 = $('<span/>', {
+	    'class':'text',
+            'text':userName
+	  });
+	  $(td2).append(span2);
+	  $(tr).append(td2);
+
+	  var td3 = $('<td/>');
+	  var span3 = $('<span/>', {
+	    'class':'text',
+            'text':firstName
+	  });
+	  $(td3).append(span3);
+	  $(tr).append(td3);
+
+	  var td4 = $('<td/>');
+	  var span4 = $('<span/>', {
+	    'class':'text',
+            'text':lastName
+	  });
+	  $(td4).append(span4);
+	  $(tr).append(td4);
+
+	  var td5 = $('<td/>');
+	  var a5 = $('<a/>', {
+	    'class':'text',
+            'href':'javascript:void(0);',
+            'text':email
+	  });
+	  $(td5).append(a5);
+	  $(tr).append(td5);
+
+        });
+
 	$('#userSelector').appendTo("body");	
 	$('#userSelector').modal('show');
+
+      },
+      error: function(){
+      }
+    });
+    
+    //$('#userSelector').appendTo("body");
+    //$('#userSelector').modal('show');
+  };
+
+  Utils.prototype.openGroupPermission = function(elem, modalId) {
+    var $videoAdminApplication = $('#videocalls-alert');
+    console.log($videoAdminApplication);
+    var url = $(elem).attr("link");
+
+
+    $.ajax({
+      url: url,
+      dataType: "json",
+      context: this,
+      success: function(data){
+	$('#groupSelector').appendTo("body");	
+	$('#groupSelector').modal('show');
 
         $.each(data, function (index, value) {
 	  var obj = jQuery.parseJSON(value);
@@ -99,10 +186,8 @@ function Utils() {
       },
       error: function(){
       }
-    });
-    
-    //$('#userSelector').appendTo("body");
-    //$('#userSelector').modal('show');
+    });  
+   
   };
 
 };
