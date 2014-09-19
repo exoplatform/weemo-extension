f799f778b0bea8fd4b15a43f4c90005ca8ec08b0
Pham Duy Dong <dongpd@exoplatform.com>
2014-09-19 17:52:30 +0700

    CHAT-92 Remove unnecessary snack-min.js from source base

--- weemo-extension-webapp/src/main/java/org/exoplatform/portlet/videocall/package-info.java
+++ weemo-extension-webapp/src/main/java/org/exoplatform/portlet/videocall/package-info.java
@@ -12,9 +12,8 @@
         location = AssetLocation.SERVER,
         scripts = {
             @Script(src = "js/jquery-1.8.3.min.js", id = "jquery"),
-            @Script(src = "js/snack-min.js", id = "snack"),
             @Script(src = "js/jquery-juzu-utils-0.1.0.js", depends = "jquery", id = "juzu-utils"),
-            @Script(src = "js/notif.js", id = "notif", depends = {"jquery", "snack", "juzu-utils"})
+            @Script(src = "js/notif.js", id = "notif", depends = {"jquery", "juzu-utils"})
         },
         stylesheets = {            
             @Stylesheet(src = "css/style.css")
